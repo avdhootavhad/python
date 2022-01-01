8220dd3487b5e3d9dcb95f28e320152bef476b4b
avdhootavhad <86868681+avdhootavhad@users.noreply.github.com>
2022-01-01 11:12:37 +0530

    Create string_buitin_functions.py

---
+++ string_buitin_functions.py
@@ -0,0 +1,32 @@
+# to use inbuild function format is :- string.method()
+a = "     Learn Any programing language to boost your skills in 21s century      "
+# you have to pass arguments to the count function
+print(a.count("n"))
+# you don't need to pass any parameter to this function
+print(a.upper())
+# you don't need to pass any parameter to this function
+print(a.lower())
+# you don't need to pass any parameter to this function
+print(a.capitalize())
+# you don't need to pass any parameter to this function
+print(a.title())
+# you have pass parameter in terms of string of which you want to fine index
+print(a.index("lang"))
+#you need pass parameter to find function
+print(a.find("boost"))
+
+#boolean
+print(a.islower())
+print(a.isupper())
+print(a.isalpha())
+print(a.isalnum())
+print(a.isdigit())
+# strip function
+b ="      Avd  ho  ot Avhad  "
+#it will remove white spaces at the beginning and end
+print(b.strip())
+#it will remove white spaces from end left side
+print(b.lsstrip())
+#it will remove white spaces from end right side
+print(b.rstrip())
+
