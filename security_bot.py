f4f6a47423fcfef571369fa5ebb62009bbba66bf
avdhootavhad <86868681+avdhootavhad@users.noreply.github.com>
2022-01-02 20:46:09 +0530

    Create security_bot.py

---
+++ security_bot.py
@@ -0,0 +1,8 @@
+a = ["avdhoot","harish","sujit","Jitendra"]
+while True:
+    user = input("enter ur name ")
+    if user in a:
+        print("ur allowed")
+    else:
+        print("ur not allowed")
+  
